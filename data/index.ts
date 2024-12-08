9d9aec84edcd0db733136a5236c06b04daba51b1
whileTrueFalse <117916649+whileTrueFalse@users.noreply.github.com>
2024-12-08 18:06:45 +0530

    added missing files

---
+++ data/index.ts
@@ -0,0 +1,223 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, ensuring a smooth backdoor for open comms.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[40vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "grid.svg",
+      spareImg: "b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a  2d cricket game-app",
+      description: "Cric-stars",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "b5.svg",
+      spareImg: "grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Smart Ambulance Detection System",
+      des: "Smart system to detect ambulance’s and clear traffic for smooth passage in emergency,using CV and YOLO",
+      img: "p1.svg",
+      iconLists: ["re.svg", "tail.svg", "ts.svg", "three.svg", "fm.svg"],
+      link: "https://github.com/whileTrueFalse/AmbulanceDetection.git",
+    },
+    {
+      id: 2,
+      title: "AI-DOC",
+      des: "AI-based doctor to provide diets,medicines based on the entered disease",
+      img: "p2.svg",
+      iconLists: ["next.svg", "tail.svg", "ts.svg", "stream.svg", "c.svg"],
+      link: "https://medidoc-t242.onrender.com",
+    },
+    {
+      id: 3,
+      title: "AI-based ANTI-VIRUS",
+      des: "AI-based Anti-virus to detect malicious files and documents at the root level using DL",
+      img: "p3.svg",
+      iconLists: ["re.svg", "tail.svg", "ts.svg", "three.svg", "c.svg"],
+      link: "https://github.com/whileTrueFalse/Titan-guard.git",
+    },
+    {
+      id: 4,
+      title: "Llama3-voice assistant",
+      des: "Voice assistant based on llama model that effectively communicates to user queries.",
+      img: "p4.svg",
+      iconLists: ["next.svg", "tail.svg", "ts.svg", "three.svg", "gsap.svg"],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Threat Intelligence Analyst.",
+      name: "ArcX",
+    //   title: "Director of AlphaStream Technologies",
+      link: "https://arcx.io/verify-certificate?id=9613457b8f83251c18f4c2f2af9d06a6c1d33b46&k=987eb71c4dd7427682d0b03364ea357b"
+    },
+    {
+      quote:
+        "Foundations of Cybersecurity.",
+      name: "Google",
+    //   title: "Director of AlphaStream Technologies",
+      link: "https://coursera.org/share/dd3ac6e1439aac2d85cf8e3403a45b57"
+    },
+    {
+      quote:
+        "The Future of Payment Technologies.",
+      name: "University of Michigan",
+    //   title: "Director of AlphaStream Technologies",
+      link: "https://coursera.org/share/3a83e9938405acc0441651b29f67e983"
+    },
+    {
+      quote:
+        "Google AI essentials.",
+      name: "Google",
+    //   title: "Director of AlphaStream Technologies",
+      link: "https://coursera.org/share/594b08c418fb2c1781fb656fed0b086d"
+    },
+    // {
+    //   quote:
+    //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    //   name: "Michael Johnson",
+    //   title: "Director of AlphaStream Technologies",
+    // },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "Tutlelr",
+      img: "cloud.svg",
+      nameImg: "cloudName.svg",
+      link:"https://www.canva.com/design/DAGXxja7ymA/si41iduDW5T7OlZpJAVFVA/view?utm_content=DAGXxja7ymA&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+    },
+    {
+      id: 2,
+      name: "LuminLabs",
+      img: "app.svg",
+      nameImg: "appName.svg",
+      link: "https://lumin-labs.dora.run"
+    },
+    // {
+    //   id: 3,
+    //   name: "HOSTINGER",
+    //   img: "/host.svg",
+    //   nameImg: "/hostName.svg",
+    // },
+    // {
+    //   id: 4,
+    //   name: "stream",
+    //   img: "/s.svg",
+    //   nameImg: "/streamName.svg",
+    // },
+    // {
+    //   id: 5,
+    //   name: "docker.",
+    //   img: "/dock.svg",
+    //   nameImg: "/dockerName.svg",
+    // },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Cybersecurity Intern",
+      desc: "Conducted vulnerability assessments and security audits to identify and mitigate risks incritical systems.",
+      className: "md:col-span-2",
+      thumbnail: "exp1.svg",
+    },
+    // {
+    //   id: 2,
+    //   title: "Mobile App Dev - JSM Tech",
+    //   desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp2.svg",
+    // },
+    // {
+    //   id: 3,
+    //   title: "Freelance App Dev Project",
+    //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp3.svg",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Lead Frontend Developer",
+    //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    //   className: "md:col-span-2",
+    //   thumbnail: "/exp4.svg",
+    // },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "git.svg",
+    },
+    {
+      id: 2,
+      img: "twit.svg",
+    },
+    {
+      id: 3,
+      img: "link.svg",
+    },
+  ];
